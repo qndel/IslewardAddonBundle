8b934dd134d1f07a9c80c7087ac823ca6f72e901
qndel <stefan551@o2.pl>
2018-10-07 23:38:45 +0200

    timestamp

---
+++ timestamp.js
@@ -0,0 +1,16 @@
+var version = "1.0";
+addons.register({
+    init: function(events) {
+        events.on('onGetMessages', this.onGetMessages.bind(this));
+    },
+    onGetMessages: function(obj) {
+        if(obj.messages){
+            if(!obj.messages[0]){
+                obj.messages = [obj.messages];
+            }
+            if(obj.messages[0].message){
+                obj.messages[0].message = "["+(new Date().toLocaleTimeString()).split(":").join("&#58;")+"] "+obj.messages[0].message;
+            }
+        }
+    }
+});
