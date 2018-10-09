5d04c0bad12ea43a83cd224feade167adaade2eb
qndel <stefan551@o2.pl>
2018-10-09 19:32:13 +0200

    testing new script system (forgot to save addondata)

--- addondata.js
+++ addondata.js
@@ -4,5 +4,6 @@ window.addonBundle = [{addonName: "Timestamp", shortName:"timestamp", url:"times
                    {addonName: "BetterRuneDescriptions", shortName:"better rune info", url:"expandedrunedescriptions.js", hoverText: "shows the min/max stat range for runes"},
                    {addonName: "QuickReply", shortName:"quick reply", url:"quickreply.js", hoverText: "allows to reply to last whisper by writing /r"},
                    {addonName: "Minimap", shortName:"minimap", url:"minimap.js", hoverText: "Press N to see minimap."},
-                   {addonName: "BossRespawn", shortName:"mogresh respawn", url:"respawntimer.js", hoverText: "Shows time to next Mogresh respawn in the events section."}
+                   {addonName: "BossRespawn", shortName:"mogresh respawn", url:"respawntimer.js", hoverText: "Shows time to next Mogresh respawn in the events section."},
+				   {addonName: "CombatLog", shortName:"combat log", url:"combatlog.js", hoverText: "Displays combat info on chat."}
                   ];
